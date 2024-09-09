505dfdb2378e4b88294147a57937afa6081271ea
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-09 15:02:55 +0300

    minor fix

--- app/client/page.js
+++ app/client/page.js
@@ -4,22 +4,26 @@ import { useState } from "react";
 const Client = () => {
   const [count, setCount] = useState(0);
   return (
-    <div className="join join-vertical">
-      <span className="countdown font-mono text-6xl mx-auto">
-        <span style={{ "--value": `${count}` }}></span>
-      </span>
-      <div className="join">
-        <button className="btn btn-primary mx-2" disabled={count === 99} onClick={() => setCount(count + 1)}>
-          +
-        </button>
-        <button className="btn btn-primary mx-2" disabled={count === 0} onClick={() => setCount(0)}>
-          0
-        </button>
-        <button className="btn btn-primary mx-2" disabled={count === 0} onClick={() => setCount(count - 1)}>
-          -
-        </button>
+    <>
+      <h1 className="text-5xl mb-8 font-bold">Client Page</h1>
+
+      <div className="join join-vertical">
+        <span className="countdown font-mono text-6xl mx-auto">
+          <span style={{ "--value": `${count}` }}></span>
+        </span>
+        <div className="join">
+          <button className="btn btn-primary mx-2" disabled={count === 0} onClick={() => setCount(count - 1)}>
+            -
+          </button>
+          <button className="btn btn-primary mx-2" disabled={count === 0} onClick={() => setCount(0)}>
+            0
+          </button>
+          <button className="btn btn-primary mx-2" disabled={count === 99} onClick={() => setCount(count + 1)}>
+            +
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
