505ed0d66abb8fddcbc84b968f909293a1dbb49b
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-09 14:56:54 +0300

    improved counter

--- app/client/page.js
+++ app/client/page.js
@@ -4,14 +4,21 @@ import { useState } from "react";
 const Client = () => {
   const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1 className="text-7xl mb-4 font-bold">{count}</h1>
-      <button className="btn btn-primary" disabled={count === 99}  onClick={() => setCount(count + 1)}>
-        +
-      </button>
-      <button className="btn btn-primary" disabled={count === 0} onClick={() => setCount(count - 1)}>
-        -
-      </button>
+    <div className="join join-vertical">
+      <span className="countdown font-mono text-6xl mx-auto">
+        <span style={{ "--value": `${count}` }}></span>
+      </span>
+      <div className="join">
+        <button className="btn btn-primary mx-2" disabled={count === 99} onClick={() => setCount(count + 1)}>
+          +
+        </button>
+        <button className="btn btn-primary mx-2" disabled={count === 0} onClick={() => setCount(0)}>
+          0
+        </button>
+        <button className="btn btn-primary mx-2" disabled={count === 0} onClick={() => setCount(count - 1)}>
+          -
+        </button>
+      </div>
     </div>
   );
 };
