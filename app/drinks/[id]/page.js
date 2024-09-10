3d8b35ad517bcd32930d32c35968bee0a1fcf667
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-10 14:15:02 +0300

    added single drink page + list grid

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,4 +1,26 @@
-const SingleDrinkPage = ({ params }) => {
-  return <div>{params.id}</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+const getSingleDrink = async (id) => {
+  const response = await fetch(url + id);
+  if (!response.ok) throw new Error(`Failed to fetch data for drink id:${id} : ${response.status}`);
+  const { drinks } = await response.json();
+  return drinks[0];
+};
+
+const SingleDrinkPage = async ({ params }) => {
+  const drinkData = await getSingleDrink(params.id);
+  const { strDrink: title, strDrinkThumb } = drinkData;
+  return (
+    <div>
+      <h1 className="text-4xl mb-8">{title}</h1>
+      <Image priority className="w-48 h-48 rounded-lg shadow-lg mb-4" width={300} height={300} src={strDrinkThumb} alt={title} />
+      <Link className="btn btn-primary mt-8 mb-12" href="/drinks">
+        Back
+      </Link>
+    </div>
+  );
 };
 export default SingleDrinkPage;
--- app/drinks/_components/DrinkList.jsx
+++ app/drinks/_components/DrinkList.jsx
@@ -1,16 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const DrinkList = ({ drinks }) => {
   const dynamicDrinksList = drinks.map((drink) => {
     return (
-      <li key={drink.idDrink}>
-        <Link className="capitalize text-xl font-medium" href={`/drinks/${drink.idDrink}`}>
-          {drink.strDrink}
-        </Link>
-      </li>
+      <Link className="join join-item bg-base-200 p-4 rounded-lg " key={drink.idDrink} href={`/drinks/${drink.idDrink}`}>
+        <Image className="w-16 h-16 rounded-lg" src={drink.strDrinkThumb} alt={drink.strDrink} width={100} height={100} />
+        <h6 className="text-lg m-auto">{drink.strDrink}</h6>
+      </Link>
     );
   });
 
-  return <ul className="menu menu-horizontal p-0">{dynamicDrinksList}</ul>;
+  return <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">{dynamicDrinksList}</div>;
 };
 export default DrinkList;
