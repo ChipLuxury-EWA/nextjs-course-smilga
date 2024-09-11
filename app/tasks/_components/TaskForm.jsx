f144debd602eb602abb140153b834cfb025f88b9
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-11 13:45:09 +0300

    added create new user feature

--- app/tasks/_components/TaskForm.jsx
+++ app/tasks/_components/TaskForm.jsx
@@ -1,4 +1,24 @@
+import prisma from "@/utils/db";
+import { revalidatePath } from "next/cache";
+
+const createTask = async (formData) => {
+  "use server";
+  const content = formData.get("content");
+  await prisma.task.create({
+    data: { content },
+  });
+
+ revalidatePath("/tasks");
+};
+
 const TaskForm = () => {
-  return <div>new task form</div>;
+  return (
+    <form action={createTask}>
+      <div className="join w-full">
+        <input className="input input-bordered join-item w-full" name="content" required placeholder="Type here..." type="text" />
+        <button className="btn btn-primary join-item">Add</button>
+      </div>
+    </form>
+  );
 };
 export default TaskForm;
