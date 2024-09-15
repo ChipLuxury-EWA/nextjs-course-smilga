c95691559acd5eb72d893972e230012311e235e9
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-15 17:32:50 +0300

    fix layout bug

--- app/tasks/_components/DeleteTask.jsx
+++ app/tasks/_components/DeleteTask.jsx
@@ -2,9 +2,11 @@ import { deleteTask } from "@/utils/task.actions";
 
 const DeleteTask = ({ id }) => {
   return (
-    <form action={deleteTask} >
+    <form className="flex gap-2" action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-error btn-xs" type="submit">Delete</button>
+      <button className="btn btn-error btn-xs" type="submit">
+        Delete
+      </button>
     </form>
   );
 };
