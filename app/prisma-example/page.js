77436f3b9d9966bf71ced5752660bcf8619ce54e
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-09-16 13:52:55 +0300

    setup stuff for build

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,11 +1,11 @@
 import prisma from "@/utils/db";
 
 const prismaHandlers = async () => {
-  await prisma.task.create({
-    data: {
-      content: "test123",
-    },
-  });
+  // await prisma.task.create({
+  //   data: {
+  //     content: "test123",
+  //   },
+  // });
 
   const allTasks = await prisma.task.findMany({
     orderBy: {
@@ -26,6 +26,9 @@ const PrismaExamplePage = async () => {
       </div>
     );
   });
+
+  if (tasks.length === 0) return <h2 className="text-2xl mt-8 font-medium text-lg">No tasks found</h2>;
+
   return (
     <div>
       <h1 className="text-5xl mb-8 font-bold">All tasks:</h1>
